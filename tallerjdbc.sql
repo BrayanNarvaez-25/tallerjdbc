9ec4a4f996208ae4907f25c99df32ec847c8571e
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-05-18 22:17:21 -0500

    Conexión PostgreSQL implementada

---
+++ tallerjdbc.sql
@@ -0,0 +1,17 @@
+-- Verificar cual base de datos se está usando
+SELECT current_database();
+
+DROP TABLE vehiculos;
+
+-- PARTE 3: Crear la tabla
+CREATE TABLE vehiculos(
+	placa VARCHAR(10) PRIMARY KEY,
+	marca VARCHAR(50) NOT NULL,
+	modelo VARCHAR(50) NOT NULL,
+	anio INT NOT NULL,
+	precio DOUBLE PRECISION NOT NULL,
+	color VARCHAR(30),
+	disponible BOOLEAN NOT NULL
+);
+
+SELECT * FROM vehiculos;
